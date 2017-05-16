217924d937b2c3528f8dcff01ab735933217322a
touilmt5 <touilmalek@gmail.com>
2017-05-16 11:20:42 +0200

    Add files via upload

---
+++ Meteo.java
@@ -0,0 +1,54 @@
+package m�t�oa;
+import java.io.*;
+import java.net.*;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.File;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+public class Meteo {
+	public static String tagMeteo(){
+		String tagPluie="";
+		try{
+			String chaine="";
+			
+			//RECUPERATION DONNEES METEO
+			URL url = new URL("https://api.darksky.net/forecast/ea4c8dfa83dc8a5d148ce6ea2237a019/58.968360,5.734090?exclude=daily,minutely,hourly");
+			URLConnection con=url.openConnection();
+			System.out.println(con.getContent());
+			InputStream input = con.getInputStream();
+			while(input.available()>0){
+				chaine+=(char)input.read();  // �crire une ligne dans le fichier resultat.txt
+			} 
+    
+			//RECUPERATION DE LA CHAINE
+			Pattern p = Pattern.compile(".*\"summary\"(.*)\"icon\".*");
+			Matcher m = p.matcher(chaine);
+			
+				//System.out.println(m.group(1));
+			
+			//DETECTION DU MOT RAIN DANS LA CHAINE
+			String [] mots = {"Rain","rain","Drizzle","drizzle"};
+			
+			for (int i=0; i<mots.length; i++){
+				int pos = chaine.indexOf(mots[i]);
+				if (pos >= 0) 
+					tagPluie="pluie";
+			}
+		}
+		catch(MalformedURLException e){System.out.println(e);}
+		catch(IOException e){System.out.println(e);}
+		catch(PatternSyntaxException pse){System.err.println("Le pattern n'a pas un format correct.");}
+		return tagPluie;
+	}
+	
+	
+	
+	
+	
+	
+
+
+}
